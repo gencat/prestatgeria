fddfadcce5d44de8ca5d053ea5891637366072cd
Toni Ginard <aginard@xtec.cat>
2014-01-09 11:35:55 +0100

    Added works.php

---
+++ html/works.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once 'config/config.php';
+
+$dbhost = $ZConfig['DBInfo']['databases']['default']['host'];
+$dbuname = $ZConfig['DBInfo']['databases']['default']['user'];
+$dbpass = $ZConfig['DBInfo']['databases']['default']['password'];
+$dbname = $ZConfig['DBInfo']['databases']['default']['dbname'];
+
+$dbc = new mysqli($dbhost, $dbuname, $dbpass, $dbname);
+
+if ($dbc->connect_errno > 0) {
+    echo 'Error ' . $dbc->connect_errno . ': ' . $dbc->connect_error;
+} else {
+    // Charset is not really required, but it's fair setting it
+    $dbc->set_charset('utf8');
+
+    $sql = 'SELECT uname FROM users LIMIT 1';
+
+    if (!$result = $dbc->query($sql)) {
+        echo 'Error ' . $dbc->connect_errno . ': ' . $dbc->connect_error;
+    } else {
+        $row = $result->fetch_assoc();
+        if (!empty($row['uname'])) {
+            // If this point is reached, everything is Ok!
+            echo 'Aplicacio:OK';
+        } else {
+            echo 'No s\'ha trobat cap registre a la taula d\'usuaris';
+        }
+    }
+
+    $dbc->close();
+}
+
