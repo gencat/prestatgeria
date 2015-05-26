cc4f56fec10253441ab766f60cf2c03c42c4ec05
josep <deyzaguirre3@gmail.com>
2015-05-26 10:09:33 +0200

    Fixed bugs in EPUB file exportation

--- html/prestatgeria/modules/Books/includes/utils.php
+++ html/prestatgeria/modules/Books/includes/utils.php
@@ -8,20 +8,41 @@
 	*/
 	function copydir($dirsource,$dirdest)
 	{
-		if(is_dir($dirsource)) {
-			$dir_handle=opendir($dirsource);
-		}
-		
-		mkdir($dirdest);
-		
-		while($file=readdir($dir_handle)) {
-			if($file!="." && $file!="..") {
-				if(!is_dir($dirsource."/".$file)) copy ($dirsource."/".$file, $dirdest. "/" .$file);
-				else copydir($dirsource."/".$file, $dirdest."/".$file);
-			}
-		} 
-		closedir($dir_handle); 
-		return true; 
+            //XTEC ************ MODIFICAT - avoid  readdir throw a warning if null param given.It breaks exportation
+            //2015.05.25 @author - Josep Caballero
+            if(is_dir($dirsource)) {
+		$dir_handle=opendir($dirsource);
+            }else{
+                mkdir($dirdest);
+                $dir_handle = opendir($dirdest);
+            }
+            
+            while($file=readdir($dir_handle)) {
+                    if($file!="." && $file!="..") {
+                            if(!is_dir($dirsource."/".$file)) copy ($dirsource."/".$file, $dirdest. "/" .$file);
+                            else copydir($dirsource."/".$file, $dirdest."/".$file);
+                    }
+            } 
+            closedir($dir_handle); 
+            return true;
+            //************ ORIGINAL
+            /*
+            if(is_dir($dirsource)) {
+                    $dir_handle=opendir($dirsource);
+            }
+
+            mkdir($dirdest);
+
+            while($file=readdir($dir_handle)) {
+                    if($file!="." && $file!="..") {
+                            if(!is_dir($dirsource."/".$file)) copy ($dirsource."/".$file, $dirdest. "/" .$file);
+                            else copydir($dirsource."/".$file, $dirdest."/".$file);
+                    }
+            } 
+            closedir($dir_handle); 
+            return true;
+            */ 
+            //************ FI
 	}
 
 ?>
