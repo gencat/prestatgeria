97114e4f391b8144c48a676bba10417622448921
Toni Ginard <toni.ginard@gmail.com>
2015-09-22 14:09:02 +0200

    Added checks for symlinks

--- html/testapp.php
+++ html/testapp.php
@@ -36,4 +36,17 @@ for ($i = 1; $i <= $env_db[$env]; $i++) {
     checkMySQL($presta['dbhost'], 3306, $presta['dbname'] . $i, $presta['dbuser'], $presta['dbpass']);
 }
 
+// Check symlinks
+echo '<h1>Check symlinks</h1>';
+passthru("ls -l $dirroot/centres"); echo '<br />';
+passthru("du -skh $dirroot/centres/"); echo '<br /><br />';
+passthru("ls -l $dirroot/rss"); echo '<br />';
+passthru("du -skh $dirroot/rss/"); echo '<br /><br />';
+passthru("ls -l $dirroot/prestatgeria/ztemp"); echo '<br />';
+passthru("du -skh $dirroot/prestatgeria/ztemp/"); echo '<br /><br />';
+passthru("ls -l $dirroot/pmf/images"); echo '<br />';
+passthru("du -skh $dirroot/pmf/images/"); echo '<br /><br />';
+passthru("ls -l $dirroot/pmf/templates_c"); echo '<br />';
+passthru("du -skh $dirroot/pmf/templates_c/"); echo '<br /><br />';
+
 test_server();
