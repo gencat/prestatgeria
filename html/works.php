ba64adda8b5e44dc480a32dc3b3a0ae194fb2f38
Toni Ginard <toni.ginard@gmail.com>
2016-02-23 14:34:25 +0100

    WORKS: Added check for data directory

--- html/works.php
+++ html/works.php
@@ -6,11 +6,12 @@ $dbhost = $presta['dbhost'];
 $dbuname = $presta['dbuser'];
 $dbpass = $presta['dbpass'];
 $dbname = $presta['dbname'];
+$state = '';
 
 $dbc = new mysqli($dbhost, $dbuname, $dbpass, $dbname);
 
 if ($dbc->connect_errno > 0) {
-    echo 'Error ' . $dbc->connect_errno . ': ' . $dbc->connect_error;
+    $state .= 'Error ' . $dbc->connect_errno . ': ' . $dbc->connect_error;
 } else {
     // Charset is not really required, but it's fair setting it
     $dbc->set_charset('utf8');
@@ -18,17 +19,26 @@ if ($dbc->connect_errno > 0) {
     $sql = 'SELECT uname FROM users LIMIT 1';
 
     if (!$result = $dbc->query($sql)) {
-        echo 'Error ' . $dbc->connect_errno . ': ' . $dbc->connect_error;
+        $state .= 'Error ' . $dbc->connect_errno . ': ' . $dbc->connect_error;
     } else {
         $row = $result->fetch_assoc();
         if (!empty($row['uname'])) {
             // If this point is reached, everything is Ok!
-            echo 'Aplicacio:OK';
+            $state .= 'Aplicacio:OK';
         } else {
-            echo 'No s\'ha trobat cap registre a la taula d\'usuaris';
+            $state .= 'No s\'ha trobat cap registre a la taula d\'usuaris';
         }
     }
 
     $dbc->close();
 }
 
+// Check one of the data directories
+$dirToCheck = $dirroot . '/centres/';
+
+if (!is_writable($dirToCheck)) {
+    $state = ($state == 'Aplicacio:OK') ? '' : $state;
+	$state .= "<br />No s'ha pogut accedir al sistema de fitxers muntat via NFS. Reviseu el punt de muntatge: $dirToCheck";
+}
+
+echo $state;
